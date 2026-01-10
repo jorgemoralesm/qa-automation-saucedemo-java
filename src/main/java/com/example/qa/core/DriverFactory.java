ce193c1e35457415490325c0679d88727a28dfc3
Jorge Morales <jorgemorales83@yahoo.es>
2026-01-10 22:01:56 +0100

    Fix: add Chrome flags for CI headless

--- src/main/java/com/example/qa/core/DriverFactory.java
+++ src/main/java/com/example/qa/core/DriverFactory.java
@@ -15,9 +15,14 @@ public class DriverFactory {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
+
         if (Config.headless()) {
             options.addArguments("--headless=new");
+            // Recomendados para CI/Linux
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
         }
+
         options.addArguments("--window-size=1280,800");
 
         WebDriver driver = new ChromeDriver(options);
